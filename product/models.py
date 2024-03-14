28d4ea24153f631e3a0f9c7a4b2e2f2fda9324fc
rasulow <rasulov.olympusss@gmail.com>
2024-03-14 21:47:56 +0500

    category subcategory superubcategory is updated

--- product/models.py
+++ product/models.py
@@ -4,10 +4,16 @@ from django.db import models
 class BaseCategory(models.Model):
     name_tm = models.CharField(max_length=100)
     name_ru = models.CharField(max_length=100)
-    icon = models.ImageField(upload_to='category-icons/')
-    slug = models.SlugField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True, blank=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    icon = models.ImageField(upload_to='category-icons/', blank=True, null=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    
+    class Meta:
+        abstract = True
+        
+    def __str__(self):
+        return self.name_tm
     
     
     
@@ -19,3 +25,24 @@ class Category(BaseCategory):
         ordering = ['-created_at']
         
         
+        
+class SubCategory(BaseCategory):
+    category = models.ForeignKey(Category, related_name='subcategories', on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'subcategory'
+        verbose_name = 'Subcategory'
+        verbose_name_plural = 'Subcategories'
+        ordering = ['-created_at']
+        
+        
+        
+class SuperSubCategory(BaseCategory):
+    category = models.ForeignKey(Category, related_name='supersubcategories', on_delete=models.CASCADE)
+    subcategory = models.ForeignKey(SubCategory, related_name='supersubcategories', on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'supersubcategory'
+        verbose_name = 'Supersubcategory'
+        verbose_name_plural = 'Supersubcategories'
+        ordering = ['-created_at']
--- product/serializers.py
+++ product/serializers.py
@@ -1,29 +1,48 @@
 from rest_framework import serializers
-from .models import Category
+from .models import *
+
+
+def localize_data(data, request):
+    if request and 'Accept-Language' in request.headers:
+        lang = request.headers['Accept-Language']
+        if lang.startswith('ru'):
+            data['name'] = data.pop('name_ru', None)
+            data.pop('name_tm', None)
+        elif lang.startswith('tk'):
+            data['name'] = data.pop('name_tm', None)
+            data.pop('name_ru', None)
+    return data
+
+
+class SuperSubCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SuperSubCategory
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        return localize_data(data, self.context.get('request'))
+
+
+class SubCategorySerializer(serializers.ModelSerializer):
+    supersubcategories = SuperSubCategorySerializer(many=True)
+
+    class Meta:
+        model = SubCategory
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        return localize_data(data, self.context.get('request'))
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    subcategories = SubCategorySerializer(many=True)
+
     class Meta:
         model = Category
         fields = '__all__'
-        
+
     def to_representation(self, instance):
-        request = self.context.get('request')
         data = super().to_representation(instance)
-        if request and 'Accept-Language' in request.headers:
-            lang = request.headers['Accept-Language']
-            if lang.startswith('ru'):
-                data['name'] = data.pop('name_ru', None)
-                data.pop('name_tm', None)
-            elif lang.startswith('tk'):
-                data['name'] = data.pop('name_tm', None)
-                data.pop('name_ru', None)
-        ordered_data = {
-            'id': data.get('id'),
-            'name': data.get('name'),
-            'icon': data.get('icon'),
-            'slug': data.get('slug'),
-            'created_at': data.get('created_at'),
-            'updated_at': data.get('updated_at'),
-        }
-        return ordered_data
+        return localize_data(data, self.context.get('request'))
